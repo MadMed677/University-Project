e74c1d140eff7d6cea59c1c8fbd647d232928eaa
MadMed677 <madmed677@gmail.com>
2015-11-19 16:56:31 +0300

    Fix webpack server error

--- client/app.js
+++ client/app.js
@@ -1,1 +1,11 @@
-console.log('hello client');
+import React                            from 'react';
+import ReactDOM                         from 'react-dom';
+import { Router, Route, Link }          from 'react-router';
+
+import Main                             from './src/main/components/Main.js';
+
+ReactDOM((
+    <Router>
+        <Route path="/" component={ Main }></Route>
+    </Router>
+), document.getElementById('root'));
--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log('hello server');
+'use strict';
+
+import express                      from 'express';
+
+let app                             = express();
+
+app.get('*', (req, res) => {
+    res.send('ok');
+});
+
+app.listen(4000);
