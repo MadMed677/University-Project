0f42f47014eb009ba4ac32eb6bf3ff33a13ee12b
MadMed677 <madmed677@gmail.com>
2015-11-29 03:39:06 +0300

    Create migration 'categories' with same DB. Create model 'Category'.

---
+++ app/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+     * Set Model "Category" to "categories" table
+     *
+     */
+    protected $table = 'categories';
+
+    /**
+     * What user can change in "Category" model
+     *
+     */
+    protected $fillable = [
+        'title'
+    ];
+}
