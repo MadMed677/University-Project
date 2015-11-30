baf1d20ebaf2a1e7e8f712193c0aa32984278abc
MadMed677 <madmed677@gmail.com>
2015-11-30 20:28:27 +0300

    Add admin lte theme

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,13 +4,27 @@
     <meta charset="UTF-8">
     <base href="/"/>
     <title>Document</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/AdminLTE.min.css">
+    <link rel="stylesheet" href="css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body ng-controller="BodyCtrl">
+<body ng-controller="BodyCtrl" class="hold-transition skin-blue sidebar-mini">
 
-    <navigation></navigation>
-    <ui-view></ui-view>
+    <div class="wrapper">
+
+        <header-directive></header-directive>
+
+        <navigation></navigation>
+
+        <div class="content-wrapper">
+            <ui-view></ui-view>
+        </div>
+    </div>
+
 
     <script src="js/app.js"></script>
+    <script src="js/vendor.js"></script>
 </body>
 </html>
--- src/js/directives/index.js
+++ src/js/directives/index.js
@@ -1,5 +1,6 @@
 export default ngModule => {
 
     require('./helpers/navigation.js')(ngModule);
+    require('./helpers/header-directive.js')(ngModule);
 
 };
