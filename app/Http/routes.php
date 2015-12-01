39dcc440b733f7b519741aae949d0e820c53db62
MadMed677 <madmed677@gmail.com>
2015-12-01 17:17:09 +0300

    Add route to logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,7 @@
 Route::get('/', function () { return view('index'); });
 Route::resource('/api/v1/activities', 'ActivityController');
 Route::resource('/api/v1/user', 'UserController');
+Route::get('/api/v1/logout', 'UserController@logout');
 
 Route::controllers([
     'auth' => 'Auth\AuthController',
